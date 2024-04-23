cd7b8a205fa91ff294749650e453d31e2f030b73
scb5304 <scb5304@gmail.com>
2024-04-23 19:24:11 -0400

    Support random name on start

--- gilbert.js
+++ gilbert.js
@@ -8,6 +8,10 @@ const token = process.env.DISCORD_TOKEN;
 const channelNamesMap = require('./names.json')
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+let changeNameOnStartup = true;
+let currentDay = days[new Date().getDay()]
+let targetChannel;
+
 client.once(Events.ClientReady, onConnected);
 client.login(token);
 
@@ -17,10 +21,12 @@ function onConnected(readyClient) {
 	let channelManager = readyClient.channels
 	channelManager.fetch('709288174526136326')
 		.then(channel => {
-			console.log(channel.name)
-			let currentDay = days[new Date().getDay()]
+			targetChannel = channel;
+			console.log(targetChannel.name)
 			console.log("It's " + currentDay)
-
+			if (changeNameOnStartup) {
+				setChannelToRandomName(currentDay)
+			}
 			let minutes = 60
 			let minutesInMillis = minutes * 60 * 1000;
 			setInterval(checkChannel, minutesInMillis);
@@ -33,15 +39,19 @@ function checkChannel() {
 	if (potentiallyNewDay != currentDay) {
 		currentDay = potentiallyNewDay
 		console.log("It's now " + currentDay)
-		let namesPool = channelNamesMap[currentDay]
-		var randomNumber = getRandomInt(0, namesPool.length - 1)
-
-		let newChannelName = channelNamesMap[currentDay][randomNumber]
-		console.log("Setting channel name to " + newChannelName)
-		channel.setName(newChannelName).then(function() {
-			console.log("I did it!")
-		}).catch(console.error);
+		setChannelToRandomName()
 	}
+}
+
+function setChannelToRandomName() {
+	let namesPool = channelNamesMap[currentDay]
+	var randomNumber = getRandomInt(0, namesPool.length - 1)
+
+	let newChannelName = channelNamesMap[currentDay][randomNumber]
+	console.log("Setting channel name to " + newChannelName)
+	targetChannel.setName(newChannelName).then(function() {
+		console.log("I did it!")
+	}).catch(console.error);
 }
 
 // https://stackoverflow.com/a/1527820/4672234
